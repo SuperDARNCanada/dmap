253134672763b3285087486c59c9d77061d327b3
Remington Rohel <rar129@usask.ca>
2023-04-28 17:11:24 +0000

    Created small test files and update integration tests to verify read/write of them.

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,36 +1,64 @@
 use dmap;
 use itertools::izip;
-use std::fs::File;
+use std::fs::{File, remove_file};
 use std::path::Path;
 
 #[test]
-fn read_iqdat() {
-    let file = File::open(Path::new("tests/test_files/")).expect("Test file not found");
-    let _contents = dmap::read_records(file);
-}
-
-#[test]
 fn test_read_write_iqdat() {
-    let file = File::open(Path::new("tests/test_files/")).expect("Test file not found");
-    let contents = dmap::read_records(file).unwrap();
+    let file = File::open(Path::new("tests/test_files/test.iqdat"))
+        .expect("test file not found");
+    let contents = dmap::read_records(file).expect("unable to read test file contents");
 
-    dmap::to_file("tests/test_files/test.iqdat", &contents).unwrap();
-    let test_file = File::open("tests/test_files/test.iqdat").expect("Test file unwritten");
-    let test_contents = dmap::read_records(test_file).unwrap();
+    dmap::to_file("tests/test_files/temp.iqdat", &contents).expect("unable to write to file");
+    let test_file = File::open("tests/test_files/temp.iqdat").expect("test file unwritten");
+    let test_contents = dmap::read_records(test_file).expect("unable to read temp file contents");
     for (read_rec, written_rec) in izip!(contents.iter(), test_contents.iter()) {
         assert_eq!(read_rec, written_rec)
     }
+    remove_file("tests/test_files/temp.iqdat").expect("Unable to delete file");
+}
+
+#[test]
+fn test_read_write_rawacf() {
+    let file = File::open(Path::new("tests/test_files/test.rawacf"))
+        .expect("test file not found");
+    let contents = dmap::read_records(file).expect("unable to read test file contents");
+
+    dmap::to_file("tests/test_files/temp.rawacf", &contents).expect("unable to write to file");
+    let test_file = File::open("tests/test_files/temp.rawacf").expect("test file unwritten");
+    let test_contents = dmap::read_records(test_file).expect("unable to read temp file contents");
+    for (read_rec, written_rec) in izip!(contents.iter(), test_contents.iter()) {
+        assert_eq!(read_rec, written_rec)
+    }
+    remove_file("tests/test_files/temp.rawacf").expect("Unable to delete file");
+}
+
+#[test]
+fn test_read_write_fitacf() {
+    let file = File::open(Path::new("tests/test_files/test.fitacf"))
+        .expect("test file not found");
+    let contents = dmap::read_records(file).expect("unable to read test file contents");
+
+    dmap::to_file("tests/test_files/temp.fitacf", &contents).expect("unable to write to file");
+    let test_file = File::open("tests/test_files/temp.fitacf").expect("test file unwritten");
+    let test_contents = dmap::read_records(test_file).expect("unable to read temp file contents");
+    for (read_rec, written_rec) in izip!(contents.iter(), test_contents.iter()) {
+        assert_eq!(read_rec, written_rec)
+    }
+    remove_file("tests/test_files/temp.fitacf").expect("Unable to delete file");
 }
 
 #[test]
 fn test_read_write_map() {
-    let file = File::open(Path::new("tests/test_files/")).expect("Test file not found");
-    let contents = dmap::read_records(file).unwrap();
+    let file = File::open(Path::new("tests/test_files/test.map"))
+        .expect("test file not found");
+    let contents = dmap::read_records(file).expect("unable to read test file contents");
 
-    dmap::to_file("tests/test_files/test.map", &contents).unwrap();
-    let test_file = File::open("tests/test_files/test.map").expect("Test file unwritten");
-    let test_contents = dmap::read_records(test_file).unwrap();
+    dmap::to_file("tests/test_files/temp.map", &contents).expect("unable to write to file");
+    let test_file = File::open("tests/test_files/temp.map").expect("test file unwritten");
+    let test_contents = dmap::read_records(test_file).expect("unable to read temp file contents");
     for (read_rec, written_rec) in izip!(contents.iter(), test_contents.iter()) {
         assert_eq!(read_rec, written_rec)
     }
+    remove_file("tests/test_files/temp.map").expect("Unable to delete file");
 }
